0d1eac80482fad851d740a9aceacc5a7af1c7bce
Clems-web <clement.servais1@gmail.com>
2020-12-01 11:31:55 +0100

    finito

---
+++ app.js
@@ -0,0 +1,14 @@
+for (age = 1; age < 100; age++) {
+    if (age < 18) {
+        document.getElementById("monParagraphe").innerHTML+="le Capitaine a " + age + "ans, il est jeune" + "<br><br>";
+    }
+    else if (age <= 50) {
+        document.getElementById("monParagraphe").innerHTML+="le Capitaine a " + age + "ans, il n'est pas très vieux" + "<br><br>";
+    }
+    else if (age <= 80) {
+        document.getElementById("monParagraphe").innerHTML += "le Capitaine a " + age + "ans, il est vieux" + "<br><br>";
+    }
+    else if (age >= 80) {
+        document.getElementById("monParagraphe").innerHTML += "le Capitaine a " + age + "ans, il est sûrement mort" + "<br><br>";
+    }
+}
